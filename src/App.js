ef8f2e1211bc7754eb1a0678b72a1cd40894f691
MaximMukhin <eriwer@gmail.com>
2022-11-25 20:37:35 +0300

    recoil todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useRecoilState } from "recoil";
 import "./App.css";
 import TodoList from "./pages/TodoList";
+import {todosState} from './atoms/todos'
 
 function App() {
+  const [todos, setTodos] = useRecoilState(todosState);
   return (
     <div className="App">
       <TodoList />
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }

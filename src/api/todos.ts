1e7eceffe7f9bdff5cd20b6b5a6599940e60e6df
MaximMukhin <eriwer@gmail.com>
2023-02-07 21:46:54 +0300

    deleteTodoById

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,20 +1,30 @@
 import axios from "axios";
 
 import { API_HOST } from "./apihost";
+import { TodoModel } from "../types";
 
-export const getTodo = async () => {
+export const getTodos = async () => {
   try {
     const res = await axios.get(`${API_HOST}/todos`);
-    const body = res.data;
-    return body;
+    return res.data;
   } catch (error) {
     console.error(`Ошибка ${error}`);
   }
 };
 
-export const createTodo = async (todo) => {
+export const createTodo = async (todo: TodoModel) => {
   try {
     const res = await axios.post(`${API_HOST}/todos`, todo);
+    const body = await res.data;
+    console.log(body);
+  } catch (error) {
+    console.error(`Ошибка ${error}`);
+  }
+};
+
+export const deleteTodoById = async (id: string) => {
+  try {
+    const res = await axios.delete(`${API_HOST}/todos/${id}`);
     const body = await res.data;
     console.log(body);
   } catch (error) {
